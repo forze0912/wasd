e08d95147e0143c35c897c0fef19e13faa544728
carljames <carljames@CATHERINEs-iMac.local>
2021-09-13 17:05:48 -1000

    Test Commit

--- commands/petInfo.js
+++ commands/petInfo.js
@@ -6,6 +6,17 @@ module.exports = {
             userID: message.author.id,
             guildID: message.guild.id
         });
+        if(!petProfile) {
+            if (!petProfile) petProfile = await new Pet({
+                _id: mongoose.Types.ObjectId(),
+                userID: message.author.id,
+                guildID: message.guild.id,
+                lastedEdited: Date.now(),
+            })
+            await petProfile.save().catch(err => console.log(err));
+            message.channel.send(`You dont have a pet pls choose one`)
+            return;
+        }
 
         if(!petProfile.Pet) return  message.channel.send('You need a pet, pls choose one, dog, cat, air,rock, hamster, hedgehog')  
         
